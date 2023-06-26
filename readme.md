cab84c4e5958c459656565ca969818be04133494
Said Ucar <msaiducar@gmail.com>
2023-06-26 19:39:26 +0200

    add isAnagram method and initial tests

--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 # beyonnex.io coding task
 
-TBA.
+Write a Java program that checks if two texts are anagrams of each other.
+
+> An _anagram_ is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
 
 ## How to use
 
--- src/main/java/io/beyonnex/Main.java
+++ src/main/java/io/beyonnex/Main.java
@@ -1,7 +1,18 @@
 package io.beyonnex;
 
+import java.util.Objects;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  public boolean isAnagram(char[] first, char[] second) {
+    Objects.requireNonNull(first, "First text shouldn't be null");
+    Objects.requireNonNull(second, "Second text shouldn't be null");
+
+    if (first.length != second.length) return false;
+
+    throw new RuntimeException();
+  }
 }

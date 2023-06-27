05d1559494fd83728d8c9c05dad5e3b3c4165d0c
Said Ucar <msaiducar@gmail.com>
2023-06-28 01:13:06 +0200

    add usage as cli app

--- src/main/java/io/beyonnex/Main.java
+++ src/main/java/io/beyonnex/Main.java
@@ -1,18 +1,25 @@
 package io.beyonnex;
 
-import java.util.Objects;
-
 public class Main {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    if (args.length != 2) {
+      printUsage();
+      return;
+    }
+
+    AnagramChecker checker = new AnagramChecker();
+    try {
+      boolean isAnagram = checker.isAnagram(args[0], args[1]);
+      System.out.printf("Given texts are%s anagram to each other%n", isAnagram ? "" : " not");
+      System.out.printf("1st : %s%n", args[0]);
+      System.out.printf("2nd : %s%n", args[1]);
+    } catch (InvalidInputException e) {
+      System.out.println(e.getMessage());
+    }
   }
 
-  public boolean isAnagram(char[] first, char[] second) {
-    Objects.requireNonNull(first, "First text shouldn't be null");
-    Objects.requireNonNull(second, "Second text shouldn't be null");
-
-    if (first.length != second.length) return false;
-
-    throw new RuntimeException();
+  private static void printUsage() {
+    System.out.println("Usage: java -jar is-anagram.jar 'dormitory' 'dirty room'");
   }
 }
